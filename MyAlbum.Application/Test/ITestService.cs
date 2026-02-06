fa667644e711beef371ae410aa07552bf960822a
bomeiweiwei <bomeilonch0210@gmail.com>
2026-02-06 20:37:11 +0800

    測試資料庫連線

---
+++ MyAlbum.Application/Test/ITestService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAlbum.Application.Test
+{
+    public interface ITestService
+    {
+        Task<bool> GetConnectResult();
+    }
+}
---
+++ MyAlbum.Application/Test/implement/TestService.cs
@@ -0,0 +1,19 @@
+﻿using MyAlbum.Domain;
+using MyAlbum.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAlbum.Application.Test.implement
+{
+    public class TestService : BaseService, ITestService
+    {
+        public TestService(IAlbumDbContextFactory factory) : base(factory) { }
+
+        public async Task<bool> GetConnectResult()
+        {
+            using var ctx = MainDB(ConnectionMode.Slave);
+            return await ctx.CanConnectAsync();
+        }
+    }
+}
