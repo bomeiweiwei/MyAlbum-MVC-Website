50a016396baafe8aa80bf93ed42f0b3d60dc020a
bomeiweiwei <bomeilonch0210@gmail.com>
2026-02-06 19:43:00 +0800

    first init

---
+++ MyAlbum.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyAlbum.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
