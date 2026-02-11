e8935d8020567fce6ad2051cec5166a66797c0b8
bomeiweiwei <bomeilonch0210@gmail.com>
2026-02-11 16:31:38 +0800

    刪除測試程式

--- MyAlbum.Web/Controllers/TestController.cs
+++ MyAlbum.Web/Controllers/TestController.cs
@@ -15,26 +15,11 @@ namespace MyAlbum.Web.Controllers
     public class TestController : ControllerBase
     {
         private readonly IWebHostEnvironment _env;
-        private readonly ITestService _testService;
-        private readonly IEmployeeAccountCreateService _employeeAccountCreateService;
-        private readonly IEmployeeAccountReadService _employeeAccountReadService;
-        private readonly IEmployeeAccountUpdateService _employeeAccountUpdateService;
-        private readonly IMemberDataUploadService _memberDataUploadService;
         public TestController(
             IWebHostEnvironment env
-            , ITestService testService
-            , IEmployeeAccountCreateService employeeAccountCreateService
-            , IEmployeeAccountReadService employeeAccountReadService
-            , IEmployeeAccountUpdateService employeeAccountUpdateService
-            , IMemberDataUploadService memberDataUploadService
             )
         {
             _env = env;
-            _testService = testService;
-            _employeeAccountCreateService = employeeAccountCreateService;
-            _employeeAccountReadService = employeeAccountReadService;
-            _employeeAccountUpdateService = employeeAccountUpdateService;
-            _memberDataUploadService = memberDataUploadService;
         }
         /// <summary>
         /// 取得環境變數
@@ -46,70 +31,6 @@ namespace MyAlbum.Web.Controllers
         {
             var environment = _env.EnvironmentName;
             return Ok(environment);
-        }
-        /// <summary>
-        /// 檢查資料庫連線
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet]
-        [Route("GetConnectResult")]
-        public async Task<ActionResult<bool>> GetConnectResult()
-        {
-            var connectResult = await _testService.GetConnectResult();
-            return Ok(connectResult);
-        }
-        /// <summary>
-        /// 測試員工新增
-        /// </summary>
-        /// <param name="req"></param>
-        /// <param name="ct"></param>
-        /// <returns></returns>
-        [HttpPost("CreateEmployeeWithAccount")]
-        [Authorize(AuthenticationSchemes = "AdminAuth")]
-        public async Task<ActionResult<bool>> CreateEmployeeWithAccount([FromBody]CreateEmployeeReq req, CancellationToken ct = default)
-        {
-            Guid result = await _employeeAccountCreateService.CreateEmployeeWithAccount(req, ct);
-            if (result != Guid.Empty)
-            {
-                return Ok(true);
-            }
-            else
-            {
-                return Ok(false);
-            }
-        }
-
-        [HttpGet("GetEmployeeAccount")]
-        public async Task<ActionResult<EmployeeAccountDto>> GetEmployeeAccount([FromQuery] GetEmployeeAccountReq req, CancellationToken ct = default)
-        {
-            var result = await _employeeAccountReadService.GetEmployeeAccountAsync(req, ct);
-            return result == null ? NotFound() : Ok(result);
-        }
-
-        [HttpGet("GetEmployeeAccountList")]
-        public async Task<ActionResult<List<EmployeeAccountDto>>> GetEmployeeAccountList([FromQuery] GetEmployeeAccountListReq req, CancellationToken ct = default)
-        {
-            var result = await _employeeAccountReadService.GetEmployeeAccountListAsync(req, ct);
-            return Ok(result);
-        }
-
-        [HttpPut("UpdateEmployeeAccount")]
-        [Authorize(AuthenticationSchemes = "AdminAuth")]
-        public async Task<ActionResult<bool>> UpdateEmployeeAccount([FromBody] UpdateEmployeeAccountReq req, CancellationToken ct = default)
-        {
-            var result = await _employeeAccountUpdateService.UpdateEmployeeAccountAsync(req, ct);
-            return Ok(result);
-        }
-
-        [HttpPost("UploadAvatar")]
-        [Authorize(AuthenticationSchemes = "MemberAuth")]
-        public async Task<IActionResult> UploadAvatar([FromForm] Guid memberId, [FromForm] IFormFile avatar, CancellationToken ct)
-        {
-            if (avatar == null || avatar.Length == 0) return BadRequest("No file");
-            await using var stream = avatar.OpenReadStream();
-            var result = await _memberDataUploadService.UploadAvatarAsync(memberId, stream, avatar.FileName, ct);
-
-            return Ok(result);
         }
     }
 }
