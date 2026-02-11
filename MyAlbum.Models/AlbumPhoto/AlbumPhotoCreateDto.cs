f603825a194eb4f1c11636b9361395a655a26bb0
bomeiweiwei <bomeilonch0210@gmail.com>
2026-02-11 11:36:25 +0800

    AlbumPhoto Repo CRUD

---
+++ MyAlbum.Models/AlbumPhoto/AlbumPhotoCreateDto.cs
@@ -0,0 +1,32 @@
+﻿using MyAlbum.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAlbum.Models.AlbumPhoto
+{
+    public sealed class AlbumPhotoCreateDto
+    {
+        public Guid AlbumPhotoId { get; set; }
+
+        public Guid AlbumId { get; set; }
+
+        public string FilePath { get; set; } = null!;
+
+        public string? OriginalFileName { get; set; }
+
+        public string? ContentType { get; set; }
+
+        public long FileSizeBytes { get; set; }
+
+        public int SortOrder { get; set; }
+
+        public int CommentNum { get; set; }
+
+        public Status Status { get; set; } = Status.Active;
+
+        public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+
+        public Guid CreatedBy { get; set; }
+    }
+}
---
+++ MyAlbum.Models/AlbumPhoto/AlbumPhotoUpdateActiveDto.cs
@@ -0,0 +1,15 @@
+﻿using MyAlbum.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAlbum.Models.AlbumPhoto
+{
+    public class AlbumPhotoUpdateActiveDto
+    {
+        public Guid AlbumPhotoId { get; set; }
+        public Status Status { get; set; }
+        public Guid UpdatedBy { get; set; }
+        public DateTime UpdatedAtUtc { get; set; }= DateTime.UtcNow;
+    }
+}
