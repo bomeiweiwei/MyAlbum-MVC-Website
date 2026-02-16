ca4ec7a878a2aa3d9f7c10654f2d9ab5f86add90
bomeiweiwei <bomeilonch0210@gmail.com>
2026-02-16 15:38:34 +0800

    顯示相簿圖片

---
+++ MyAlbum.Web/Areas/Member/Controllers/AlbumPhotoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAlbum.Application.AlbumPhoto;
+using MyAlbum.Models.Album;
+using MyAlbum.Models.AlbumPhoto;
+
+namespace MyAlbum.Web.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class AlbumPhotoController : Controller
+    {
+        private readonly IMemberAlbumPhotoReadService _read;
+        public AlbumPhotoController(
+            IMemberAlbumPhotoReadService read)
+        {
+            _read = read;
+        }
+
+        public async Task<IActionResult> Index(Guid albumId)
+        {
+            GetAlbumPhotoReq req = new GetAlbumPhotoReq();
+            req.AlbumId = albumId;
+            var dataList = await _read.GetAlbumPhotoListAsync(req);
+            return View(dataList);
+        }
+    }
+}
