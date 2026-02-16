2704523c9f45ea3ba114e5b2c138866901b5d044
bomeiweiwei <bomeilonch0210@gmail.com>
2026-02-16 19:42:06 +0800

    設定停用啟用

--- MyAlbum.Web/Areas/Member/Controllers/AlbumPhotoController.cs
+++ MyAlbum.Web/Areas/Member/Controllers/AlbumPhotoController.cs
@@ -9,10 +9,13 @@ namespace MyAlbum.Web.Areas.Member.Controllers
     public class AlbumPhotoController : Controller
     {
         private readonly IMemberAlbumPhotoReadService _read;
+        private readonly IAlbumPhotoUpdateService _update;
         public AlbumPhotoController(
-            IMemberAlbumPhotoReadService read)
+            IMemberAlbumPhotoReadService read,
+            IAlbumPhotoUpdateService update)
         {
             _read = read;
+            _update = update;
         }
 
         public async Task<IActionResult> Index(Guid albumId)
@@ -21,6 +24,51 @@ namespace MyAlbum.Web.Areas.Member.Controllers
             req.AlbumId = albumId;
             var dataList = await _read.GetAlbumPhotoListAsync(req);
             return View(dataList);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Active(UpdateMemberAlbumPhotoActiveReq model)
+        {
+            var req = new UpdateAlbumPhotoActiveReq
+            {
+                AlbumPhotoId = model.AlbumPhotoId,
+                Status = Shared.Enums.Status.Active
+            };
+
+            var ok = await _update.UpdateAlbumPhotoActiveAsync(req);
+            if (!ok)
+            {
+                TempData["Error"] = "操作失敗";
+            }
+            else
+            {
+                TempData["Success"] = "已停用成功";
+            }
+
+            return RedirectToAction("Index", new { albumId = model.AlbumId });
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Disable(UpdateMemberAlbumPhotoActiveReq model)
+        {
+            var req = new UpdateAlbumPhotoActiveReq
+            {
+                AlbumPhotoId = model.AlbumPhotoId,
+                Status = Shared.Enums.Status.Disabled
+            };
+
+            var ok = await _update.UpdateAlbumPhotoActiveAsync(req);
+            if (!ok)
+            {
+                TempData["Error"] = "操作失敗";
+            }
+            else
+            {
+                TempData["Success"] = "已停用成功";
+            }
+
+            return RedirectToAction("Index", new { albumId = model.AlbumId });
         }
     }
 }
