358c6ddfca8be221157092116785ca53d25a904d
bomeiweiwei <bomeilonch0210@gmail.com>
2026-02-16 11:31:20 +0800

    調整

---
+++ MyAlbum.Web/Areas/Member/Controllers/AlbumController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAlbum.Web.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class AlbumController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
