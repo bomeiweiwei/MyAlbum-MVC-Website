8d7c16d47b85bf913d1e56becb7c0203c478e0d0
bomeiweiwei <bomeilonch0210@gmail.com>
2026-02-17 11:25:12 +0800

    調整頁面與顯示首頁資料

---
+++ MyAlbum.Web/Models/TopImgComment/TopPhotoCommentViewModel.cs
@@ -0,0 +1,9 @@
+﻿using MyAlbum.Models.AlbumPhoto;
+
+namespace MyAlbum.Web.Models.TopImgComment
+{
+    public class TopPhotoCommentViewModel
+    {
+        public List<AlbumPhotoDto> Photos { get; set; } = new List<AlbumPhotoDto>();
+    }
+}
