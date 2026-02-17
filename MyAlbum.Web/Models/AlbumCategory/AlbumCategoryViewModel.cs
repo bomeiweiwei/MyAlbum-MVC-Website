7813c8f292ac341b3e47225867d9d2078a13f113
bomeiweiwei <bomeilonch0210@gmail.com>
2026-02-17 12:56:00 +0800

    顯示類別圖片與調整熱門圖片

---
+++ MyAlbum.Web/Models/AlbumCategory/AlbumCategoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using MyAlbum.Models.AlbumPhoto;
+
+namespace MyAlbum.Web.Models.AlbumCategory
+{
+    public class AlbumCategoryViewModel
+    {
+        public string CategoryName { get; set; } = null!;
+        public List<AlbumPhotoDto> Photos { get; set; } = new List<AlbumPhotoDto>();
+    }
+}
