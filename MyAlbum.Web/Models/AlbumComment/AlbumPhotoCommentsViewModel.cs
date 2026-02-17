9ede9fc43c44093d2f25ce1f3132b44353ed9bef
bomeiweiwei <bomeilonch0210@gmail.com>
2026-02-17 14:25:10 +0800

    新增留言

---
+++ MyAlbum.Web/Models/AlbumComment/AlbumPhotoCommentsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using MyAlbum.Models.AlbumComment;
+using MyAlbum.Models.AlbumPhoto;
+
+namespace MyAlbum.Web.Models.AlbumComment
+{
+    public class AlbumPhotoCommentsViewModel
+    {
+        public AlbumPhotoDto Photo { get; }
+        public List<AlbumCommentDto> Comments { get; }
+
+        public AlbumPhotoCommentsViewModel(
+            AlbumPhotoDto photo,
+            List<AlbumCommentDto>? comments = null)
+        {
+            Photo = photo ?? throw new ArgumentNullException(nameof(photo));
+            Comments = comments ?? new List<AlbumCommentDto>();
+        }
+    }
+}
